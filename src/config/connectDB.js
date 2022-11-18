f54b36f3657b185cd53d45e5d7f7bb4088bd804d
namdv01 <khonghoatay@gmail.com>
2022-11-18 10:06:46 +0700

    change remote database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("ecommerce", "root", '123456', {
-  host: "localhost",
+const sequelize = new Sequelize('railway', 'root', 'CCDxIOIFjySLIvX4WZRk', {
+  host: 'containers-us-west-112.railway.app',
   dialect: "mysql",
-});
+  port: 5833}
+);
 
 let connectDB = async () => {
   try {
